0e4d82af559aaf6570292c042ebb7dfe7c4c026c
VidhulPrasad123 <vidhul@shade6.com>
2025-12-21 19:28:18 +0530

    new upad

--- models/Signal.js
+++ models/Signal.js
@@ -1,57 +1,29 @@
-import mongoose from 'mongoose';
+const mongoose = require("mongoose");
 
 const SignalSchema = new mongoose.Schema(
   {
-    signal:{
-       type: String,
-    },
-    strength: {
-      type: String,
-      required: true,
-    },
-    price: {
-      type: Number,
-      required: true
-    },
+    signal: String,
+    strength: { type: String, required: true },
+    price: { type: Number, required: true },
     signalTime: {
       type: Date,
-      required: true,
       default: Date.now,
       index: true
     },
-    notified: {
-      type: Boolean,
-      default: false
-    },  
-    confidence:{
-      type: Number,
-      required: true
-    },
-    cisd:{
-      type:String
-    },
-    cisdLevel:{
-      type:String
-    },
-    trend:{
-      type:String
-    },
-    bullishSweep:{
-      type:String
-    },
-    bearishSweep:{
-      type:String
-    },
-    swingHigh:{
-      type:String
-    },
-    swingLow:{
-      type:String
-    }
+    notified: { type: Boolean, default: false },
+    confidence: { type: Number, required: true },
+
+    cisd: Boolean,
+    cisdLevel: String,
+    trend: String,
+    bullishSweep: Boolean,
+    bearishSweep: Boolean,
+    swingHigh: Number,
+    swingLow: Number
   },
-  {
-    timestamps: true
-  }
+  { timestamps: true }
 );
 
-export default mongoose.model('Signal', SignalSchema);
+module.exports = mongoose.model('Signal', SignalSchema);
+
+
